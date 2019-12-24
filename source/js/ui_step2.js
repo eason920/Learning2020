4dad389c64e0d3054d7a61541de61678dd6959ed
EasonFunday <eason@funday.asia>
2019-12-24 18:25:06 +0800

    1224-2

--- source/js/ui_step2.js
+++ source/js/ui_step2.js
@@ -9,20 +9,28 @@ $(function(){
 			clearInterval(sid);
 			const $main = $('.s2-main');
 			const height = $main.height();
+			const max = $('.s2-main-item').length;
 
 			$('.s2-main-item').css({height});
 
 			$('.s2-main-next').click(function(){
-				const top = ( $(this).parent().index() + 1 ) * height * -1;
+				const idx = $(this).parent().parent().parent().index() + 1;
+				console.log(idx);
+				if( idx < max ){
+					const top = idx * height * -1;
+					$('.s2-main-box').animate({ top });
+				}else{
+					$('.is-step-switch3').click();
+					setTimeout(function(){
+						$('.s2-main-box').animate({ top: 0 });
+					}, 500);
+				}
 				$('.s2-main-box').animate({ top });
 			});
+
 			
+			console.log(max);
 			
-
-
-
-
-
 		}
 	}
 	let sid = setInterval(ready, 50)
